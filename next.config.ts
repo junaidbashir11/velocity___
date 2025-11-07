6014d2080ad45c8aa566b0a05a209c8b39e70ca1
junaidbashir11 <iam.cypher.000@gmail.com>
2025-11-07 14:23:53 +0530

    fixlint

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  ignoreDuringBuilds: true,
+  lint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
